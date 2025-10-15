4aa10d32e408c5e5fce36356cbdcaab2392406e3
santhosh-bs22 <santhosh220506@gmail.com>
2025-10-15 23:08:31 +0530

    sample

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ecommercesample/', // 👈 use your repo name here
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') }
